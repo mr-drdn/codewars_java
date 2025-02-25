db551f1c02732f564b5625a6cac3d2b394f1416a
Artur Kotikoff <openrolla@yandex.ru>
2025-02-25 17:09:03 +0500

    4 8 15 16 23 42

--- src/main/java/org/kata/kyu_8/Square.java
+++ src/main/java/org/kata/kyu_8/Square.java
@@ -1,12 +1,16 @@
 package org.kata.kyu_8;
 
 public class Square {
+//    public static boolean isSquare(int n) {
+//        if (n < 0) {
+//            return false;
+//        }
+//        int sqt = (int) Math.sqrt(n);
+//        return sqt * sqt == n;
+//    }
+
     public static boolean isSquare(int n) {
-        if (n < 0) {
-            return false;
-        }
-        int sqt = (int) Math.sqrt(n);
-        return sqt * sqt == n;
+        return Math.sqrt(n) % 1 == 0;
     }
 
     public static void main(String[] args) {
@@ -14,8 +18,4 @@ public class Square {
     }
 }
 
-/*public class Square {
-    public static boolean isSquare(int n) {
-        return Math.sqrt(n)%1 == 0;
-    }
-}*/
+
