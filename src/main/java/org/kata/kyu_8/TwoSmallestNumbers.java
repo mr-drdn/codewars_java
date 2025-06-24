4ade6c175d8d92f704092e79f50dceefadec8c3f
Artur Kotikoff <openrolla@yandex.ru>
2025-06-24 16:11:58 +0500

    4 8 15 16 23 42

---
+++ src/main/java/org/kata/kyu_8/TwoSmallestNumbers.java
@@ -0,0 +1,23 @@
+package org.kata.kyu_8;
+
+/*
+Создайте функцию, которая возвращает сумму двух наименьших положительных чисел из массива,
+содержащего минимум 4 положительных целых числа. В массиве не будет дробных чисел или неположительных значений.
+🔹 Для Java входные числа будут представлены в виде long (числа с двойной точностью хранения).*/
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/*public class TwoSmallestNumbers {
+    public static long sumTwoSmallestNumbers(long[] numbers) {
+        System.out.println(numbers);
+        Arrays.sort(numbers);
+        System.out.println(numbers);
+        return 0L;
+    }
+
+    public static void main(String[] args) {
+        sumTwoSmallestNumbers([1,2,3,4]);
+//        sumTwoSmallestNumbers([1,23,4,5,])
+    }
+}*/
